fa27e025b3263584b87c33225ae5e120fd781657
Nathan <nathan.18421011@student.ubl.ac.idgit config --global user.name nathansandhikagit remote add origin https://github.com/kelas-pemrograman-3-informatika18/kelas-pemrograman-3-informatika18-latihan1-nathansandhika-18421011.gitgit push -u origin master$ git config --global user.email nathan.18421011@student.ubl.ac.id>
2020-03-26 18:42:14 +0700

    first commit

---
+++ src/pages/Login.vue
@@ -0,0 +1,70 @@
+<template>
+    <q-page class="bg-grey-3">
+      <div class="row">
+        <q-card class="fixed-center col-md-4 col-xs-12 bg-white" flat>
+          <q-card-section>
+            <div class="text-h6 q-pb-md">
+              Login page
+    </div>
+    <q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+    >
+      <q-input
+        filled
+        v-model="username"
+        label="Username *"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Type your username']"
+      />
+
+      <q-input
+        filled
+        type="Password"
+        v-model="password"
+        label="Password *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val.length > 0 || 'Type your password']"
+      />
+      <div>
+        <q-btn label="Enter" type="enter" color="primary"/>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+    </q-card-section>
+    </q-card>
+    </div>
+    </q-page>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      username: null,
+      password: null
+    }
+  },
+  methods: {
+    onSubmit () {
+      if (this.username === 'Nathan' && this.password === '18421011') {
+        this.$q.notify({
+          type: 'positive',
+          message: 'Login Successful!'
+        })
+        this.$router.push('/home')
+      } else {
+        this.$q.notify({
+          type: 'negative',
+          message: 'Login Failed, try again!'
+        })
+      }
+    },
+    onReset () {
+      this.username = null
+      this.password = null
+    }
+  }
+}
+</script>
---
+++ src/pages/Register.vue
@@ -0,0 +1,89 @@
+<template>
+    <q-page>
+    Register page
+    <q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+    >
+      <q-input
+        filled
+        v-model="Name"
+        label="Name *"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Type your Name']"
+      />
+
+      <q-input
+        filled
+        v-model="Username"
+        label="Username *"
+        lazy-rules
+        :rules="[val => val !== null && val.length > 0 || 'Type your Username']"
+      />
+      <q-input
+        filled
+        type="Password"
+        v-model="Password"
+        label="Password *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val.length > 0 || 'Type your Password'
+        ]"
+      />
+      <q-input
+        filled
+        v-model="CPassword"
+        label="Confirm password *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val.length > 0 || 'Confirm your password'
+        ]"
+      />
+      <q-input
+        filled
+        v-model="Address"
+        label="Address *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val.length > 0 || 'Type your Address'
+        ]"
+      />
+      <q-input
+        filled
+        v-model="Phone"
+        label="Phone *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val.length > 0 || 'Type your Phone'
+        ]"
+      />
+      <div>
+        <q-btn label="Enter" type="enter" color="primary"/>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+    </q-page>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      Name: null,
+      Username: null,
+      Password: null,
+      CPassword: null,
+      Address: null,
+      Phone: null
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('Submit')
+    },
+    onReset () {
+      console.log('Reset')
+    }
+  }
+}
+</script>
